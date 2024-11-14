cb1f7af9171710fa4650e1a9a7afe625de01a7d8
jameskimani001 <jameskim092t@gmail.com>
2024-11-14 16:52:05 +0300

    planty completed

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,14 +1,20 @@
-import React from "react";
+import React,{useState} from "react";
 
-function PlantCard({ plant }) {
-  const { name, image, price, inStock } = plant;
-
+function PlantCard({ plant , onStockClick}) {
+  const { name, image, price } = plant;
+  const[inStock, setinStock ] = useState(true);
+const handleClick = () => {
+  setinStock (!inStock)
+  if(onStockClick) {
+    onStockClick(plant.id);
+  }
+};
   return (
     <li className="card">
       <img src={image || "https://via.placeholder.com/400"} alt={name} />
       <h4>{name}</h4>
       <p>Price: ${price.toFixed(2)}</p>
-      <button className={inStock ? "primary" : ""}>
+      <button onClick={handleClick}   className={inStock ? "primary" : ""}>
         {inStock ? "In Stock" : "Out of Stock"}
       </button>
     </li>
